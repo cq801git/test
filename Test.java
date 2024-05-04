d44a067c8d87ca2a589fcf06c3e36a1b90c16f60
cq801git <tjkb@21cn.com>
2024-05-04 12:59:18 +0800

    init commit

---
+++ Test.java
@@ -0,0 +1,36 @@
+
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		//冒泡排序 		
+		int[] arr = {1,3,2,4,6,5,8};
+		
+
+
+
+
+
+		//提示用户输入  
+		 
+
+
+
+
+
+
+
+
+
+		
+
+	}
+
+	
+
+	
+
+
+
+}
---
+++ t1.js
@@ -0,0 +1,43 @@
+
+//cq1
+//两个数求和 
+
+
+
+
+
+// 定义一个名为 sayHello 的函数
+function sayHello() {
+    console.log("Hello, World!");
+}
+
+// 定义一个名为 greet 的函数，它接受一个名字参数
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
+
+// 调用 sayHello 函数
+sayHello();
+
+// 调用 greet 函数并传入一个名字参数
+greet("Alice");
+
+// 声明一些变量
+let number = 42;
+const pi = 3.14159;
+
+// 打印变量的值
+console.log("The number is:", number);
+console.log("Pi is approximately:", pi);
+
+// 使用条件语句
+if (number > 20) {
+    console.log("The number is greater than 20.");
+} else {
+    console.log("The number is less than or equal to 20.");
+}
+
+// 使用循环语句
+for (let i = 0; i < 5; i++) {
+    console.log("The value of i is:", i);
+}
